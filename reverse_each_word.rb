78884bf3a96c2396cb934a136304fc436c73323a
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-25 19:34:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,7 @@
 string = "Hello there, and how are you?"
 
 def reverse_each_word(string)
-array = string.split
-
-print array[0].reverse
-print array[1].reverse
-print array[2].reverse
-print array[3].reverse
-print array[4].reverse
-print array[5].reverse
-
-array.join
-
+string.split.collect {|word| word.reverse}.join " "
 end
 
 reverse_each_word(string)
