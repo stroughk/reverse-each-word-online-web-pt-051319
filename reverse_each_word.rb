37cf7aea7f01bf8e57edf6c81410c12fe4431829
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-25 19:27:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,17 @@
-sentence = "Hello there, and how are you?"
+string = "Hello there, and how are you?"
 
-def reverse_each_word(sentence)
-      sentence.reverse
+def reverse_each_word(string)
+array = string.split
+
+print array[0].reverse
+print array[1].reverse
+print array[2].reverse
+print array[3].reverse
+print array[4].reverse
+print array[5].reverse
+
+array.join
+
 end
 
+reverse_each_word(string)
